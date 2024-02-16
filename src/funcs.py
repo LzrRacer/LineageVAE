05ad07f15e69810c273e64abecb73ec1251ca05f
LzrRacer <90468590+LzrRacer@users.noreply.github.com>
2024-02-16 17:33:47 +0900

    Update funcs.py
    for installation

--- src/funcs.py
+++ src/funcs.py
@@ -1,6 +1,7 @@
 import torch.distributions as dist
 import random
-
+import torch
+import torch.nn.functional as F
 
 def calc_kld(qz):
     kld = -0.5 * (1 + qz.scale.pow(2).log() - qz.loc.pow(2) - qz.scale.pow(2))
@@ -11,8 +12,8 @@ def calc_poisson_loss(ld, norm_mat, obs):
     p_z = dist.Poisson(ld * norm_mat)
     l = - p_z.log_prob(obs)
     return(l)
-        
-    
+
+
 def calc_nb_loss(ld, norm_mat, theta, obs):
     ld = norm_mat * ld
     p =  ld / (ld + theta)
@@ -43,6 +44,29 @@ def calc_lptr(z_list, sigma, day):
     return(lptr)
 
 
+def take_t(t_list, day_maxs, t):
+    take_list = []
+    for i, day_max in enumerate(day_maxs):
+        day_max = day_max
+        t_idx = int(day_max - t)
+        a = t_list[t_idx][i]
+        take_list.append(a)
+    take_list = torch.stack(take_list)
+    return(take_list)
+
+
+def qz_series(qz_mu_list, qz_logvar_list, day, initial):
+    cell = 0
+    qz_series_list = []
+    for t in day:
+        t = int(t) - initial
+        for i in range(t):
+            qz = dist.Normal(qz_mu_list[i][cell], F.softplus(qz_logvar_list[i][cell]))
+            qz_series_list.append(qz)
+        cell += 1
+    return(qz_series_list)
+
+
 def stratified_sampling(day):
     stratified_list = []
     day_list = []
@@ -50,3 +74,29 @@ def stratified_sampling(day):
         a1 = random.choice([i for i, x in enumerate(day) if x == a])
         day_list.append(a1)
     return(day_list)
+
+
+def upsampling_z(qz_mu_T0, qz_logvar_T0, day, timepoint, upsample_num, z_dim):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    upsampled_day = torch.concat((day, timepoint*torch.ones(upsample_num).to(device)),0).to(device)
+    pseudo_cell_num = qz_mu_T0[day == timepoint].shape[0]
+    if pseudo_cell_num == 0:
+        qz2_mu_T0 = torch.zeros(upsample_num, z_dim).to(device)
+        qz2_logvar_T0 = torch.zeros(upsample_num, z_dim).to(device)
+        z2_T0 = torch.zeros(upsample_num, z_dim).to(device)
+    else:
+        param_list = []
+        for i in range(upsample_num):
+            param_list.append(random.randint(0, pseudo_cell_num-1))
+        qz2_mu_T0_list = []
+        qz2_logvar_T0_list = []
+        for k in param_list:
+            qz2_mu_T0_list.append(qz_mu_T0[day == timepoint][k])
+            qz2_logvar_T0_list.append(qz_logvar_T0[day == timepoint][k])
+        qz2_mu_T0 = torch.stack(qz2_mu_T0_list, dim=0).to(device)
+        qz2_logvar_T0 = torch.stack(qz2_logvar_T0_list, dim=0).to(device)
+        qz2_T0 = dist.Normal(qz2_mu_T0, qz2_logvar_T0)
+        z2_T0 = qz2_T0.rsample()
+        z2_T0 = z2_T0.to(device)
+    return(qz2_mu_T0, qz2_logvar_T0, z2_T0)
+
